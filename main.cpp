a7384f15d0517147b14cac877bdfd54bff18cb30
BLooDBRothER <computertechnology446@gmail.com>
2021-09-22 13:32:51 +0530

    create game greeting and file change

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "print.h"
+
+using namespace std;
+
+int main(){
+    cout << "Welcome to the Game HANGMAN" << endl;
+    printStatus(0);
+    cout << endl << "[1] - New Game\n[2] - Load Game";
+}
--- print.h
+++ print.h
@@ -1,2 +1,113 @@
+#include <bits/stdc++.h>
 
+using namespace std;
 
+void printStatus(int no)
+{
+    switch (no)
+    {
+    case 0:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             /|\\          " << endl;
+        cout << "|            / | \\         " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             / \\          " << endl;
+        cout << "|            /   \\         " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 1:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             /|\\          " << endl;
+        cout << "|            / | \\         " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             / \\          " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 2:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             /|\\          " << endl;
+        cout << "|            / | \\         " << endl;
+        cout << "|              |            " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 3:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             /|\\          " << endl;
+        cout << "|            / | \\         " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 4:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|             /|\\          " << endl;
+        cout << "|              |            " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 5:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 6:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|              o            " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+        break;
+    case 7:
+        cout << "o__________________________o" << endl;
+        cout << "|              |            " << endl;
+        cout << "|              |            " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|                           " << endl;
+        cout << "|___________________________" << endl;
+    }
+}
