f3533dc0909a4c429988952ad46d3232a630619d
BLooDBRothER <computertechnology446@gmail.com>
2021-09-25 09:55:57 +0530

    Update logic for start and load game

--- game.h
+++ game.h
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include "word.h"
+// #include "word.h"
 
 using namespace std;
 
@@ -7,3 +7,62 @@ bool checkUserName(string name){
     fstream file(name);
     return file.is_open() ? true : false;
 }
+class GameData{
+    private:
+        fstream userFile;
+        selectWords words;
+    public:
+        string gameWord;
+        GameData(string fileName, bool newGame){
+            if(newGame){
+                updateUserList(fileName);
+                userFile.open(fileName+".txt", ios::trunc | ios::in | ios::out);
+                int seed = time(0);
+                userFile << "word: " + to_string(seed);
+                initGame(seed);
+            }
+            else{
+                loadGame();
+            }
+        }
+
+        void loadGame(){
+            fstream file("userList.txt", ios::in);
+            vector<string> user;
+            string dummy;
+            for(int i=0; getline(file, dummy); i++){
+                cout << "[ " + to_string(i) + " ] " + dummy << endl;
+                user.push_back(dummy);
+            }
+            file.close();
+            int option;
+            cout << "Select the Game to Load:  ";
+            cin >> option;
+            while(option >= user.size()){
+                cout << "xxxx INVALID CHOICE xxxx\n";
+                cout << "Select the Game to Load:  ";
+                cin >> option;
+            }
+            userFile.open(user[option]+".txt", ios::in | ios::out);
+            userFile.seekg(6, ios::beg);
+            string seed;
+            getline(userFile, seed);
+            initGame(stoi(seed));
+        }
+
+        void initGame(int seed){
+            gameWord = words.getWord(seed);
+        }
+
+        void updateUserList(string name){
+            fstream file("userList.txt", ios::app);
+            file << name << "\n";
+            file.close();
+        }
+};
+class Game : protected GameData{
+    public:
+        Game(string fileName, bool newGame):GameData(fileName, newGame){
+            cout << gameWord;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <bits/stdc++.h>
 #include "print.h"
+#include "word.h"
+#include "game.h"
 
 using namespace std;
 
+void loadGame(){
+    Game game("_", false);
+}
+
+void newGame(){
+    string userName;
+    cout << "Enter user Name:  ";
+    cin >> userName;
+    while(checkUserName(userName+".txt")){
+        cout << "xxxx USER NAME ALREADY TAKEN XXXX" << endl;
+        cout << "Enter user Name:  ";
+        cin >> userName;
+    }
+    Game game(userName, true);
+}
+
 int main(){
+    int gameOption;
     cout << "Welcome to the Game HANGMAN" << endl;
     printStatus(0);
-    cout << endl << "[1] - New Game\n[2] - Load Game";
+    cout << endl << "[1] - New Game\n[2] - Load Game\nEnter Your choice:  ";
+    cin >> gameOption;
+    switch (gameOption)
+    {
+    case 1:
+        newGame();
+        break;
+    case 2:
+        loadGame();
+        break;
+    default:
+        break;
+    }
 }
